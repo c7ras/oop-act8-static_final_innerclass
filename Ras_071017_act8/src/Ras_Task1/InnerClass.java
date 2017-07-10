a5f8c8e5d6cb873cc9f0374342ac046ee9b36b3b
c7ras <r.chesterlloyd07@gmail.com>
2017-07-10 15:29:38 +0800

    First Commit

---
+++ Ras_071017_act8/src/Ras_Task1/InnerClass.java
@@ -0,0 +1,10 @@
+
+package Ras_Task1;
+
+public class InnerClass {
+    int data2 = 10;
+    void method(OuterClass oc){
+        System.out.println(oc.data);
+        System.out.println(data2);
+    }
+}
---
+++ Ras_071017_act8/src/Ras_Task1/OuterClass.java
@@ -0,0 +1,6 @@
+
+package Ras_Task1;
+
+public class OuterClass {
+    int data =5;
+}
